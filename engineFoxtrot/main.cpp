05305e04b99b548a636be9c045b1987874fd7ec8
LarsJ2000 <ljph.jansen@student.avans.nl>
2020-09-07 15:33:14 +0200

    Initial Project
    Initial project with SDL2

---
+++ engineFoxtrot/main.cpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include "SDL.h"
+#undef main
+using namespace std;
+
+int main() {
+
+	cout << "This is the engine project" << endl;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Foxtrot Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(5000);
+
+	return 0;
+}
