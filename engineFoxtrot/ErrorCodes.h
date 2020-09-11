bd3815c5902fd4c4ea56446a7799d0a8e3fc4d43
Rinke de Vries <Rinkedevries@gmail.com>
2020-09-11 19:53:28 +0200

    Added debug errorcodes and generalhelperfunctions

---
+++ engineFoxtrot/ErrorCodes.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifndef ERRORCODES__H
+#define ERRORCODES__H
+
+#define ERROR_CODE_something 0x01
+#define ERROR_CODE_something 0x02
+#define ERROR_CODE_something 0x03
+#define ERROR_CODE_something 0x04
+#define ERROR_CODE_something 0x05
+#define ERROR_CODE_something 0x06
+#define ERROR_CODE_something 0x07
+#define ERROR_CODE_something 0x08
+#define ERROR_CODE_something 0x09
+#define ERROR_CODE_something 0x0A
+#define ERROR_CODE_something 0x0B
+#define ERROR_CODE_something 0x0C
+#define ERROR_CODE_something 0x0D
+#define ERROR_CODE_something 0x0E
+#define ERROR_CODE_something 0x0F
+#define ERROR_CODE_something 0x10
+#define ERROR_CODE_something 0x11
+#define ERROR_CODE_something 0x12
+#define ERROR_CODE_something 0x13
+#define ERROR_CODE_something 0x14
+#define ERROR_CODE_something 0x15
+#define ERROR_CODE_something 0x16
+#define ERROR_CODE_something 0x17
+
+#endif
