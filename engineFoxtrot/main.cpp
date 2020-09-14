2c1a5c3166c5ddc5b613e9fd066596f8e5186e2b
Rinke de Vries <Rinkedevries@gmail.com>
2020-09-14 13:05:05 +0200

    Main changed, included box2d

--- engineFoxtrot/main.cpp
+++ engineFoxtrot/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include "Engine.h"
 
+#include "../box2d-master/include/box2d/box2d.h"
+using namespace std;
 // Doxygen
 // Cntrl + / above function or class to generate Doxygen
 // https://devblogs.microsoft.com/cppblog/doxygen-and-xml-doc-comment-support/
@@ -14,14 +16,19 @@ Engine engine;
 
 int main() {
 
-	engine.svi.load("../Assets/Sound/b423b42.wav");
-	engine.svi.play();
-	engine.svi.testLoopVideo();
-	
-	while (1)
-	{
-		engine.svi.input();
-	}
+	b2Vec2 gravity(0.0f, -10.0f);
+	b2World world(gravity);
+
+	cout << "Hello world!" << endl;
+	return 0;
+	//engine.svi.load("../Assets/Sound/b423b42.wav");
+	//engine.svi.play();
+	//engine.svi.testLoopVideo();
+	//
+	//while (1)
+	//{
+	//	engine.svi.input();
+	//}
 }
 
 
