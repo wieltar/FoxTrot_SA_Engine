239b31d733dc29f5413a71fbcad740beaadc0954
LarsJ2000 <ljph.jansen@student.avans.nl>
2020-09-14 14:04:06 +0200

    Added box2d
    Added box2d and converted SDL2 to x64 now build on x64

--- engineFoxtrot/main.cpp
+++ engineFoxtrot/main.cpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <iostream>
-#include "Engine.h"
+//#include "Engine.h"
 
-#include "../box2d-master/include/box2d/box2d.h"
+#include "box2d/box2d.h"
 using namespace std;
+
 // Doxygen
 // Cntrl + / above function or class to generate Doxygen
 // https://devblogs.microsoft.com/cppblog/doxygen-and-xml-doc-comment-support/
@@ -12,7 +13,7 @@ using namespace std;
 
 // TODO : Set object folder up with interface route
 
-Engine engine;
+//Engine engine;
 
 int main() {
 
@@ -21,6 +22,9 @@ int main() {
 
 	cout << "Hello world!" << endl;
 	return 0;
+
+
+
 	//engine.svi.load("../Assets/Sound/b423b42.wav");
 	//engine.svi.play();
 	//engine.svi.testLoopVideo();
