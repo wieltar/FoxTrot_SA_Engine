45da921763c7b475b643d669d4208a2edd173dd7
Rinke de Vries <Rinkedevries@gmail.com>
2020-09-21 16:23:06 +0200

    Clean main of box2d example

--- engineFoxtrot/main.cpp
+++ engineFoxtrot/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include "Engine.h"
 
-#include "box2d/box2d.h"
 using namespace std;
 
 // Doxygen
@@ -16,59 +15,6 @@ using namespace std;
 Engine engine;
 
 int main() {
-
-	b2Vec2 gravity(0.0f, 10.0f);
-	b2World world(gravity);
-
-	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(0.0f, 130.0f);
-
-	b2Body* groundBody = world.CreateBody(&groundBodyDef);
-
-	b2PolygonShape groundBox;
-	groundBox.SetAsBox(50.0f, 10.0f);
-
-	groundBody->CreateFixture(&groundBox, 0.0f);
-
-
-
-
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 0.0f);
-	b2Body* body = world.CreateBody(&bodyDef);
-
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(1.0f, 1.0f);
-
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
-	fixtureDef.density = 10.0f;
-	fixtureDef.friction = 0.3f;
-
-	fixtureDef.restitution = 0.9f;
-
-	body->CreateFixture(&fixtureDef);
-
-	float timeStep = 1.0f / 60.0f;
-
-	int32 velocityIterations = 6;
-	int32 positionIterations = 2;
-
-	for (int32 i = 0; i < 1000; ++i)
-	{
-		world.Step(timeStep, velocityIterations, positionIterations);
-		b2Vec2 position = body->GetPosition();
-		float angle = body->GetAngle();
-		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-		engine.svi.testLoopVideo((int)position.x, (int)position.y);
-		SDL_Delay(10);
-	}
-
-	cout << "Hello world!" << endl;
-	return 0;
-
-
 
 	//engine.svi.load("../Assets/Sound/b423b42.wav");
 	//engine.svi.play();
