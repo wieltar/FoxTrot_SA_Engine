ff43b8def4d367cf64f58a3efa2b32070e2a4f3d
Rinke de Vries <Rinkedevries@gmail.com>
2020-09-28 15:38:10 +0200

    Added values to enum. Should never be empty

--- engineFoxtrot/EventType.h
+++ engineFoxtrot/EventType.h
@@ -2,17 +2,17 @@
 
 enum EventCategory
 {
-	None = 0,
-	MOUSE_EVENT,
-	WINDOW_EVENT,
-	INPUT_EVENT,
-	ENGINE_EVENT
+	None			= 0x01,
+	MOUSE_EVENT		= 0x02,
+	WINDOW_EVENT	= 0x03,
+	INPUT_EVENT		= 0x04,
+	ENGINE_EVENT	= 0x05
 };
 
 enum class EventType {
-	MOVE,
-	INTERACT,
-	INPUT,
-	MOVE_UPDATED,
-	COLLISION
+	MOVE			= 0x01,
+	INTERACT		= 0x02,
+	INPUT			= 0x03,
+	MOVE_UPDATED	= 0x04,
+	COLLISION		= 0x05
 };
