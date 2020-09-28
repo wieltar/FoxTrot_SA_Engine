3a7d05861026d01610aa4b5c92c813712cd7ab35
Rinke de Vries <Rinkedevries@gmail.com>
2020-09-28 21:46:42 +0200

    Extended main

--- engineFoxtrot/main.cpp
+++ engineFoxtrot/main.cpp
@@ -29,21 +29,21 @@ int main() {
 	thread t1(engineTick,std::ref(engine));
 	t1.detach();
 	
-	engine.createNewSceneWithSceneID(10);
+	engine.createNewSceneWithSceneID(2);
 	engine.createNewSceneWithSceneID(1);
 
 	engine.createNewObjectWithSceneID(1, 1, 50, 5, 40, 40);
 
-	engine.createNewObjectWithSceneID(10, 1, 50, 5, 40, 40);
-	engine.createNewObjectWithSceneID(10, 2, 100, 5, 40, 40);
-	engine.createNewObjectWithSceneID(10, 3, 150, 5, 40, 40);
-	engine.createNewObjectWithSceneID(10, 4, 5, 50, 40, 40);
-	engine.createNewObjectWithSceneID(10, 5, 50, 5, 40, 40);
-	engine.createNewObjectWithSceneID(10, 6, 100, 50, 40, 40);
-	engine.createNewObjectWithSceneID(10, 7, 150, 50, 40, 40);
-	engine.createNewObjectWithSceneID(10, 8, 5, 6, 40, 40);
-	engine.createNewObjectWithSceneID(10, 9, 5, 100, 40, 40);
-	engine.createNewObjectWithSceneID(10, 10, 50, 100, 40, 40);
+	engine.createNewObjectWithSceneID(2, 1, 50, 5, 40, 40);
+	engine.createNewObjectWithSceneID(2, 2, 100, 5, 40, 40);
+	engine.createNewObjectWithSceneID(2, 3, 150, 5, 40, 40);
+	engine.createNewObjectWithSceneID(2, 4, 5, 50, 40, 40);
+	engine.createNewObjectWithSceneID(2, 5, 50, 5, 40, 40);
+	engine.createNewObjectWithSceneID(2, 6, 100, 50, 40, 40);
+	engine.createNewObjectWithSceneID(2, 7, 150, 50, 40, 40);
+	engine.createNewObjectWithSceneID(2, 8, 5, 6, 40, 40);
+	engine.createNewObjectWithSceneID(2, 9, 5, 100, 40, 40);
+	engine.createNewObjectWithSceneID(2, 10, 50, 100, 40, 40);
 
 	engine.linkSpriteIDWithAssetPath(1, "../Assets/Sprites/Project assets/Adventurer-1.5/Individual Sprites/adventurer-air-attack1-00.png");
 	engine.linkSpriteIDWithAssetPath(2, "../Assets/Sprites/Project assets/Adventurer-1.5/Individual Sprites/adventurer-air-attack1-01.png");
@@ -56,11 +56,17 @@ int main() {
 	engine.linkSpriteIDWithAssetPath(9, "../Assets/Sprites/Project assets/Adventurer-1.5/Individual Sprites/adventurer-attack1-04.png");
 	engine.linkSpriteIDWithAssetPath(10, "../Assets/Sprites/Project assets/Adventurer-1.5/Individual Sprites/helmet_02a.png");
 	
-	engine.setCurrentScene(10);
+	engine.setCurrentScene(2);
 	
 	SDL_Delay(5000);
+	for (int i = 0; i < 100; i++)
+	{
+		if(i % 2)engine.setCurrentScene(1);
+		else engine.setCurrentScene(2);
+		SDL_Delay(50);
+	}
+	
 	engine.setCurrentScene(1);
-	SDL_Delay(1000);
 	
 	for (int i = 0; i < 100; i++)
 	{
@@ -71,7 +77,7 @@ int main() {
 	for (int i = 0; i < 360; i++)
 	{
 		engine.setObjectRotation(1, i);
-		SDL_Delay(50);
+		SDL_Delay(10);
 	}
 
 
