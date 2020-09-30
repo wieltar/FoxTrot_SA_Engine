fca118f27f4aaabd83f458b275a347ffa303107d
WilliamRossAvans <44771477+WilliamRossAvans@users.noreply.github.com>
2020-09-30 15:40:39 +0200

    between

--- engineFoxtrot/main.cpp
+++ engineFoxtrot/main.cpp
@@ -3,6 +3,7 @@
 #include "Engine.h"
 
 #include "box2d/box2d.h"
+#include <chrono>
 using namespace std;
 
 // Doxygen
@@ -16,32 +17,133 @@ using namespace std;
 Engine engine;
 int speed =1 ;
 
+int remainingLeftSteps = 0;
+int remainingRightSteps = 0;
+
+bool movingJump = false;
+bool movingLeft = false;
+bool movingRight = false;
+
+
 void moveLeft(b2Body* body) {
 	b2Vec2 position = body->GetPosition();
-	position.x -= speed;
+	if (remainingLeftSteps < 40) {
+		//position.x -= speed;
+		body->ApplyForce(b2Vec2(0.0f, 10.0f), position, true); 
+	}
+	else {
+		position.x -= speed * 2;
+	}
 	body->SetTransform(position, body->GetAngle());
+	remainingLeftSteps++;
 }
 void moveRight(b2Body* body) {
 	b2Vec2 position = body->GetPosition();
-	position.x += speed;
+	if (remainingRightSteps < 40) {
+		position.x += speed;
+
+		//body->ApplyLinearImpulse(b2Vec2(0, 0.0), position, true);
+	}
+	else {
+		position.x += speed * 2;
+		//body->ApplyLinearImpulse(b2Vec2(20.0, 0.0), position, true);
+	}
 	body->SetTransform(position, body->GetAngle());
+	remainingRightSteps++;
+}
+void moveJump(b2Body* body) {
+	body->ApplyLinearImpulse(b2Vec2(0, -400), body->GetPosition(), true);
+}
+void moveJumpLeft(b2Body* body) {
+	body->ApplyLinearImpulse(b2Vec2(-100, -400), body->GetPosition(), true);
+}
+void moveJumpRight(b2Body* body) {
+	body->ApplyLinearImpulse(b2Vec2(100, -400), body->GetPosition(), true);
+}
+void stopJump(b2Body* body) {
+	body->ApplyLinearImpulse(b2Vec2(0, 0), body->GetPosition(), true);
 }
 
-void moveStep(int i, b2Body* body) {
-	if (i > 300 && i < 400) {
-		moveRight(body);
+
+/*
+move right 
+jump straigt up
+jump right
+jump left
+*/
+void moveCommandos(int i) {
+	if (i == 200) {
+		movingRight = true;
 	}
-	else if (i > 401 && i < 600) {
-		moveLeft(body);
+	if (i == 300) {
+		movingRight = false;
 	}
-	else if (i > 601 && i < 900) {
-		moveRight(body);
+
+	if (i == 400) {
+		movingJump = true;
 	}
+	if (i == 500) {
+		movingJump = false;
+	}
+
+	if (i == 1000) {
+		movingJump = true;
+		movingLeft = true;
+	}
+	if (i == 1100) {
+		movingJump = false;
+		movingLeft = false;
+	}
+
+	/*
+	if (i == 1100) {
+		movingJump = true;
+		movingRight = true;
+	}
+	if (i == 1200) {
+		movingJump = false;
+		movingRight = false;
+	}*/
+}
+
+void move(b2Body* body) {
+	if (movingJump) {
+		if (movingRight) {
+			moveJumpRight(body);
+		}
+		else if (movingLeft) {
+			moveJumpLeft(body);
+		}
+		else {
+			moveJump(body);
+		}
+	}
+	else {
+		stopJump(body);
+
+		if (movingLeft)
+			moveLeft(body);
+		else if (remainingLeftSteps > 0) {
+			moveLeft(body);
+			remainingLeftSteps = remainingLeftSteps - 2;
+		}
+
+		if (movingRight)
+			moveRight(body);
+		else if (remainingRightSteps > 0) {
+			if (remainingRightSteps > 40) {
+				remainingRightSteps = 40;
+			}
+			moveRight(body);
+			remainingRightSteps = remainingRightSteps - 2;
+		}
+	}
+
 }
 
 int main() {
 
-	b2Vec2 gravity(0.0f, 10.0f);
+	b2Vec2 gravity(0.0f, 80.0f);
 	b2World world(gravity);
 
 	b2BodyDef groundBodyDef;
@@ -53,9 +155,6 @@ int main() {
 	groundBox.SetAsBox(50.0f, 10.0f);
 
 	groundBody->CreateFixture(&groundBox, 0.0f);
-
-
-
 
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
@@ -78,14 +177,19 @@ int main() {
 
 	int32 velocityIterations = 6;
 	int32 positionIterations = 2;
-
-	for (int32 i = 0; i < 1000; ++i)
+	b2Body* temp;
+	for (int32 i = 0; i < 10000; ++i)
 	{
 		world.Step(timeStep, velocityIterations, positionIterations);
-		moveStep(i, body);
+		moveCommandos(i);//input handlings
+		temp = world.GetBodyList();
+		while (temp) {
+			move(temp);
+			temp = temp->GetNext();
+		}
 		b2Vec2 position = body->GetPosition();
 		float angle = body->GetAngle();
-		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+		printf("%4.2f %4.2f %4.2f \n", position.x, position.y, angle);
 		engine.svi.testLoopVideo(position.x, position.y);
 		SDL_Delay(10);
 	}
