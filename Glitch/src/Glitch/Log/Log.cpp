73f13ca5a6b0e110299c5ddb183f6576ff8d157b
Wouter van Hees <whjhees@avans.nl>
2020-10-03 16:24:07 +0200

    glitch proj included in engine foxtrot'

---
+++ Glitch/src/Glitch/Log/Log.cpp
@@ -0,0 +1,17 @@
+#include "glpch.h"
+#include "Log.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+namespace Glitch {
+
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+
+	void Log::Init() {
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		s_CoreLogger = spdlog::stdout_color_mt("ENGINE");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+		s_CoreLogger = spdlog::stdout_color_mt("APP");
+		s_CoreLogger->set_level(spdlog::level::trace);
+	}
+}
---
+++ Glitch/src/Glitch/Log/Log.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "glpch.h"
+#include "Glitch/Core.h"
+#include "spdlog/spdlog.h"
+#include <spdlog/fmt/ostr.h>
+
+namespace Glitch {
+	class GLITCH_API Log
+	{
+	public:
+		static void Init();
+
+		static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+	};
+}
+
+#define GL_CORE_FATAL(...)	::Glitch::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define GL_CORE_ERROR(...)	::Glitch::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define GL_CORE_WARN(...)	::Glitch::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define GL_CORE_INFO(...)	::Glitch::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define GL_CORE_TRACE(...)	::Glitch::Log::GetCoreLogger()->trace(__VA_ARGS__)
+
+#define GL_FATAL(...)	::Glitch::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define GL_ERROR(...)	::Glitch::Log::GetClientLogger()->error(__VA_ARGS__)
+#define GL_WARN(...)	::Glitch::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define GL_INFO(...)	::Glitch::Log::GetClientLogger()->info(__VA_ARGS__)
+#define GL_TRACE(...)	::Glitch::Log::GetClientLogger()->trace(__VA_ARGS__)
+
