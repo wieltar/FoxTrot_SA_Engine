9b3cc2f61a0a52b2dbac4cf60688a0f61378dcb5
Wouter van Hees <whjhees@avans.nl>
2020-10-03 17:41:41 +0200

    added more todo

--- Glitch/src/Glitch.h
+++ Glitch/src/Glitch.h
@@ -4,6 +4,8 @@
 #include "Glitch/Log/Log.h"
 #include "Glitch/Events/Event.h"
 // TODO add things to export here
+// TODO move everything to glitch namespace
+// TODO add GLITCH_API prefix to classes which need to be exported
 
 // -- EntryPoint -----------------------------
 #include "Glitch/EntryPoint.h"
