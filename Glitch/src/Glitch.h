0457a4b54a5e62dc0e8aebf770551fd0d886c08d
Wouter van Hees <whjhees@avans.nl>
2020-10-03 17:41:34 +0200

    added todo

--- Glitch/src/Glitch.h
+++ Glitch/src/Glitch.h
@@ -3,6 +3,7 @@
 #include "Glitch//Core/Object.h"
 #include "Glitch/Log/Log.h"
 #include "Glitch/Events/Event.h"
+// TODO add things to export here
 
 // -- EntryPoint -----------------------------
 #include "Glitch/EntryPoint.h"
