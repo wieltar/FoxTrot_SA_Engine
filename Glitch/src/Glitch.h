a30ca5cb85c230fea817c81836251dbb0355b24d
Wouter van Hees <whjhees@avans.nl>
2020-10-04 13:28:02 +0200

    Update Glitch.h

--- Glitch/src/Glitch.h
+++ Glitch/src/Glitch.h
@@ -3,21 +3,22 @@
 #include "Glitch/Core/Application.h"
 #include "Glitch/Core/Object.h"
 #include "Glitch/Core/ErrorCodes.h"
-// ---------------------------------------------
 #include "Glitch/Log/Log.h"
 #include "Glitch/Events/Event.h"
+// ---------------------------------------------
 // -- SceneManager -----------------------------
 #include "Glitch/Renderer/SceneManager/SceneManager.h"
 #include "Glitch/Renderer/SceneManager/Scene.h"
 // ---------------------------------------------
-// -- SoundManager -----------------------------
+// -- SoundEngine ------------------------------
 #include "Glitch/Sound/SoundEngine.h"
 // ---------------------------------------------
 // -- EntryPoint -------------------------------
 #include "Glitch/EntryPoint.h"
 // ---------------------------------------------
-// TODO add things to export here
-// TODO move everything to glitch namespace
-// TODO Alles nalopen
+
+// TODO Physics implementeren
 // TODO Sound overzetten
 // TODO add GLITCH_API prefix to classes which need to be exported
+// TODO add things to export here
+// TODO Alles nalopen
