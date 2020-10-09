4bf051ea3ecbe0308a984a0aa286cd8554ecf772
Rinke de Vries <Rinkedevries@gmail.com>
2020-10-09 12:13:13 +0200

    Cleaned Scene + const

--- engineFoxtrot/Scene.cpp
+++ engineFoxtrot/Scene.cpp
@@ -3,9 +3,9 @@
 
 /// @brief 
 /// @param sceneID 
-Scene::Scene(int sceneID)
+Scene::Scene(const int id) : sceneID(id)
 {
-	this->sceneID = sceneID;
+
 }
 
 /// @brief 
@@ -14,15 +14,21 @@ Scene::~Scene()
 }
 
 /// @brief 
+/// Adds new object to the scene. If Object is empty function does not do anything. 
 /// @param object 
+/// Pointer to a new Object to be added to the Scene.
 void Scene::addNewObject(Object* object) 
 {
+	if (object == nullptr) return;
 	objects.push_back(object);
 }
 
 /// @brief 
+/// Function for checking if an object with ObjectID exists in a Scene.
 /// @param objectID 
+/// Identifier for a ObjectID
 /// @return 
+/// Returns true if Object is found in current scene else false.
 bool Scene::checkIfObjectExists(int objectID)
 {
 	for (Object* obj : objects)
@@ -41,7 +47,13 @@ bool Scene::checkIfObjectExists(int objectID)
 /// @param yPos 
 /// @param height 
 /// @param width 
-void Scene::addNewObject(int id, int xPos, int yPos, int height, int width,int speed, int jumpHeight, int density, int friction, int restitution, bool stat)
+/// @param speed 
+/// @param jumpHeight 
+/// @param density 
+/// @param friction 
+/// @param restitution 
+/// @param stat 
+void Scene::addNewObject(const int id, const int xPos, const int yPos, const int height, const int width, const int speed, const int jumpHeight, const int density, const int friction, const int restitution, const bool stat)
 {
 	Object* obj = new Object(id, xPos, yPos, height, width);
 	obj->setDensity(density);
@@ -54,8 +66,11 @@ void Scene::addNewObject(int id, int xPos, int yPos, int height, int width,int s
 }
 
 /// @brief 
+/// Returns the object matched with the given objectID, if none are found an error is thrown
 /// @param ObjectID 
+/// Identifier for objectID
 /// @return 
+/// Returns pointer to the found Object
 Object * Scene::getObject(int objectID)
 {
 	for (Object * obj : objects)
