264fe7c0ad5757361ca7ab333f4d422a376fd9ec
Thijs <ttleeuw96@gmail.com>
2020-10-29 16:42:10 +0100

    fix?

--- engineFoxtrot/Engine/Physics/ContactListenerAdapter.h
+++ engineFoxtrot/Engine/Physics/ContactListenerAdapter.h
@@ -40,7 +40,7 @@ protected:
 		auto result = facade->getObjectsByFixture(fixtureOne, fixtureTwo);
 		if (result.object1 != nullptr && result.object2 != nullptr) {
 			auto manifold = contact->GetManifold();
-			contact->SetEnabled(true);
+
 			auto f = contact->GetFriction();
 			auto object1 = result.object1;
 			auto object2 = result.object2;
@@ -49,47 +49,79 @@ protected:
 			float oldX = 0;
 			bool shouldCheckTopDown = false;
 			bool shouldCheckRightLeft = false;
- 			for (auto e : contact->GetManifold()->points) {
-				if (oldY == e.localPoint.y) {
-					shouldCheckTopDown = true;
-				}
-				if (oldX == e.localPoint.x) {
-					shouldCheckRightLeft = true;
-				}
-				oldY = e.localPoint.y;
-				oldX = e.localPoint.x;
-			}
+
+			b2WorldManifold worldManifold;
+			contact->GetWorldManifold(&worldManifold);
+			b2Vec2 collPoint = worldManifold.points[0];
 
 			std::map<int, Direction> direction = std::map<int, Direction>();
+			if (object1->getPositionY() < collPoint.y) {
+				direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::DOWN));
+			}
+			else if (object1->getPositionY() > collPoint.y) {
+				direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::UP));
+			}
+			if (object1->getPositionX() < collPoint.x) {
+				direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::LEFT));
+			}
+			else if (object1->getPositionX() > collPoint.x) {
+				direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::RIGHT));
+			}
 
-			if (shouldCheckRightLeft) {
-				if (object1->getPositionX() < object2->getPositionX()) {
-					std::cout << "OBJECT " << object1->getSpriteID() << " right COLLISION" << std::endl;
-					std::cout << "OBJECT " << object2->getSpriteID() << " left COLLISION" << std::endl;
-					direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::LEFT));
-					direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::RIGHT));
-				}
-				if (object1->getPositionX() > object2->getPositionX()) {
-					std::cout << "OBJECT " << object1->getSpriteID() << " left COLLISION" << std::endl;
-					std::cout << "OBJECT " << object2->getSpriteID() << " right COLLISION" << std::endl;
-					direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::RIGHT));
-					direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::LEFT));
-				}
+			if (object2->getPositionY() < collPoint.y) {
+				direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::DOWN));
 			}
-			if (shouldCheckTopDown) {
-				if (object1->getPositionY() < object2->getPositionY()) {
-					std::cout << "OBJECT " << object1->getSpriteID() << " Bottom COLLISION" << std::endl;
-					std::cout << "OBJECT " << object2->getSpriteID() << " Top COLLISION" << std::endl;
-					direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::DOWN));
-					direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::UP));
-				}
-				if (object1->getPositionY() > object2->getPositionY()) {
-					std::cout << "OBJECT " << object1->getSpriteID() << " Top COLLISION" << std::endl;
-					std::cout << "OBJECT " << object2->getSpriteID() << " Bottom COLLISION" << std::endl;
-					direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::RIGHT));
-					direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::LEFT));
-				}
+			else if (object2->getPositionY() > collPoint.y) {
+				direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::UP));
 			}
+			if (object2->getPositionX() < collPoint.x) {
+				direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::LEFT));
+			}
+			else if (object2->getPositionX() > collPoint.x) {
+				direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::RIGHT));
+			}
+
+ 		//	for (auto e : contact->GetManifold()->points) {
+			//	if (oldY == e.localPoint.y) {
+			//		shouldCheckTopDown = true;
+			//	}
+			//	if (oldX == e.localPoint.x) {
+			//		shouldCheckRightLeft = true;
+			//	}
+			//	oldY = e.localPoint.y;
+			//	oldX = e.localPoint.x;
+			//}
+
+			//std::map<int, Direction> direction = std::map<int, Direction>();
+
+			//if (shouldCheckRightLeft) {
+			//	if (object1->getPositionX() < object2->getPositionX()) {
+			//		std::cout << "OBJECT " << object1->getSpriteID() << " right COLLISION" << std::endl;
+			//		std::cout << "OBJECT " << object2->getSpriteID() << " left COLLISION" << std::endl;
+			//		direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::LEFT));
+			//		direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::RIGHT));
+			//	}
+			//	if (object1->getPositionX() > object2->getPositionX()) {
+			//		std::cout << "OBJECT " << object1->getSpriteID() << " left COLLISION" << std::endl;
+			//		std::cout << "OBJECT " << object2->getSpriteID() << " right COLLISION" << std::endl;
+			//		direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::RIGHT));
+			//		direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::LEFT));
+			//	}
+			//}
+			//if (shouldCheckTopDown) {
+			//	if (object1->getPositionY() < object2->getPositionY()) {
+			//		std::cout << "OBJECT " << object1->getSpriteID() << " Bottom COLLISION" << std::endl;
+			//		std::cout << "OBJECT " << object2->getSpriteID() << " Top COLLISION" << std::endl;
+			//		direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::DOWN));
+			//		direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::UP));
+			//	}
+			//	if (object1->getPositionY() > object2->getPositionY()) {
+			//		std::cout << "OBJECT " << object1->getSpriteID() << " Top COLLISION" << std::endl;
+			//		std::cout << "OBJECT " << object2->getSpriteID() << " Bottom COLLISION" << std::endl;
+			//		direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::UP));
+			//		direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::DOWN));
+			//	}
+			//}
 			std::cout << "BEGIN === Ob1: " << result.object1->getSpriteID() << "  Obj2: " << result.object2->getSpriteID() << std::endl;
  			EventSingleton::get_instance().dispatchEvent<OnCollisionBeginEvent>((Event&)OnCollisionBeginEvent(result.object1->getSpriteID(), result.object2->getSpriteID(), direction));
 		}
@@ -105,53 +137,83 @@ protected:
 			auto object1 = result.object1;
 			auto object2 = result.object2;
 
-			int oldY = 0;
-			int oldX = 0;
-			bool shouldCheckTopDown = false;
-			bool shouldCheckRightLeft = false;
-			for (auto e : contact->GetManifold()->points) {
-				if (oldY == e.localPoint.y) {
-					shouldCheckTopDown = true;
-				}
-				if (oldX == e.localPoint.x) {
-					shouldCheckRightLeft = true;
-				}
-				oldY = e.localPoint.y;
-				oldX = e.localPoint.x;
-			}
-
+			b2WorldManifold worldManifold;
+			contact->GetWorldManifold(&worldManifold);
+			b2Vec2 collPoint = worldManifold.points[0];
 
 			std::map<int, Direction> direction = std::map<int, Direction>();
-
-			if (shouldCheckRightLeft) {
-				if (object1->getPositionX() < object2->getPositionX()) {
-					std::cout << "OBJECT " << object1->getSpriteID() << " left COLLISION" << std::endl;
-					std::cout << "OBJECT " << object2->getSpriteID() << " right COLLISION" << std::endl;
-					direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::LEFT));
-					direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::RIGHT));
-				}
-				if (object1->getPositionX() > object2->getPositionX()) {
-					std::cout << "OBJECT " << object1->getSpriteID() << " right COLLISION" << std::endl;
-					std::cout << "OBJECT " << object2->getSpriteID() << " left COLLISION" << std::endl;
-					direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::RIGHT));
-					direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::LEFT));
-				}
+			if (object1->getPositionY() < collPoint.y) {
+				direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::UP));
 			}
-			if (shouldCheckTopDown) {
-				if (object1->getPositionY() < object2->getPositionY()) {
-					std::cout << "OBJECT " << object1->getSpriteID() << " Bottom COLLISION" << std::endl;
-					std::cout << "OBJECT " << object2->getSpriteID() << " Top COLLISION" << std::endl;
-					direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::DOWN));
-					direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::UP));
-				}
-				if (object1->getPositionY() > object2->getPositionY()) {
-					std::cout << "OBJECT " << object1->getSpriteID() << " Top COLLISION" << std::endl;
-					std::cout << "OBJECT " << object2->getSpriteID() << " Bottom COLLISION" << std::endl;
-					direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::RIGHT));
-					direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::LEFT));
-
-				}
+			else if (object1->getPositionY() > collPoint.y) {
+				direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::DOWN));
 			}
+			if (object1->getPositionX() < collPoint.x) {
+				direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::LEFT));
+			}
+			else if (object1->getPositionX() > collPoint.x) {
+				direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::RIGHT));
+			}
+
+			if (object2->getPositionY() < collPoint.y) {
+				direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::UP));
+			}
+			else if (object2->getPositionY() > collPoint.y) {
+				direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::DOWN));
+			}
+			if (object2->getPositionX() < collPoint.x) {
+				direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::LEFT));
+			}
+			else if (object2->getPositionX() > collPoint.x) {
+				direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::RIGHT));
+			}
+			//int oldY = 0;
+			//int oldX = 0;
+			//bool shouldCheckTopDown = false;
+			//bool shouldCheckRightLeft = false;
+			//for (auto e : contact->GetManifold()->points) {
+			//	if (oldY == e.localPoint.y) {
+			//		shouldCheckTopDown = true;
+			//	}
+			//	if (oldX == e.localPoint.x) {
+			//		shouldCheckRightLeft = true;
+			//	}
+			//	oldY = e.localPoint.y;
+			//	oldX = e.localPoint.x;
+			//}
+
+
+			//std::map<int, Direction> direction = std::map<int, Direction>();
+
+			//if (shouldCheckRightLeft) {
+			//	if (object1->getPositionX() < object2->getPositionX()) {
+			//		std::cout << "OBJECT " << object1->getSpriteID() << " left COLLISION" << std::endl;
+			//		std::cout << "OBJECT " << object2->getSpriteID() << " right COLLISION" << std::endl;
+			//		direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::LEFT));
+			//		direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::RIGHT));
+			//	}
+			//	if (object1->getPositionX() > object2->getPositionX()) {
+			//		std::cout << "OBJECT " << object1->getSpriteID() << " right COLLISION" << std::endl;
+			//		std::cout << "OBJECT " << object2->getSpriteID() << " left COLLISION" << std::endl;
+			//		direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::RIGHT));
+			//		direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::LEFT));
+			//	}
+			//}
+			//if (shouldCheckTopDown) {
+			//	if (object1->getPositionY() < object2->getPositionY()) {
+			//		std::cout << "OBJECT " << object1->getSpriteID() << " Bottom COLLISION" << std::endl;
+			//		std::cout << "OBJECT " << object2->getSpriteID() << " Top COLLISION" << std::endl;
+			//		direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::DOWN));
+			//		direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::UP));
+			//	}
+			//	if (object1->getPositionY() > object2->getPositionY()) {
+			//		std::cout << "OBJECT " << object1->getSpriteID() << " Top COLLISION" << std::endl;
+			//		std::cout << "OBJECT " << object2->getSpriteID() << " Bottom COLLISION" << std::endl;
+			//		direction.insert(std::pair<int, Direction>(object1->getSpriteID(), Direction::UP));
+			//		direction.insert(std::pair<int, Direction>(object2->getSpriteID(), Direction::DOWN));
+
+			//	}
+			//}
 
 			std::cout << "END ==== Ob1: " << result.object1->getSpriteID() << "  Obj2: " << result.object2->getSpriteID() << std::endl;
 			EventSingleton::get_instance().dispatchEvent<OnCollisionEndEvent>((Event&)OnCollisionEndEvent(result.object1->getSpriteID(), result.object2->getSpriteID(), direction));
