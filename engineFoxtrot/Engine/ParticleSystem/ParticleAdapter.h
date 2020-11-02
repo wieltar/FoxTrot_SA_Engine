92b8b9c81d54f811a6285352accf830f1f5fe6c5
Rinke de Vries <Rinkedevries@gmail.com>
2020-11-02 15:57:32 +0100

    Added doxygen

--- engineFoxtrot/Engine/ParticleSystem/ParticleAdapter.h
+++ engineFoxtrot/Engine/ParticleSystem/ParticleAdapter.h
@@ -2,6 +2,8 @@
 #include "ParticleSystem/ParticleLib/ParticleInit.h"
 #include "SceneManager/Object.h"
 
+/// @brief 
+/// Particle Bridge that inherits from Object and ParticleInit(Cocos2dx Library)
 class ParticleAdapter : public Object, public ParticleInit
 {
 public:
